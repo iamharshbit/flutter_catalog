1ce492604f10406c1ec73906d6ba2c4e0b8f0738
iamharshbit <17bit114@nirmauni.ac.in>
2021-02-20 09:31:37 +0530

    main branch commited

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Container(
+          child:Text("30 days of flutter")
+        ),
+      ),
+    );
+  }
+}
