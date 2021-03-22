c1ad9694c434c7073496b065e3ab2eb489c447de
iamharshbit <17bit114@nirmauni.ac.in>
2021-03-22 20:35:19 +0530

    Day17 commited

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily,
+      fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: AppBarTheme(
           color: Colors.white,
           elevation: 0.0,
@@ -16,4 +16,7 @@ class MyTheme {
       ThemeData(brightness: Brightness.dark
           // primaryTextTheme: GoogleFonts.latoTextTheme(),
           );
+  //colors
+  static Color creamColor = Color(0Xfff5f5f5);
+  static Color darkBluish = Color(0xff403b58);
 }
